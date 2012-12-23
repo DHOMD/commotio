2721ee10581e36647a66b83809e1b8588c52d318
DHO <DHO@dho.im>
2012-12-23 15:31:17 -0500

    Commit 2

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+add_lunch_combo commotio_toro-userdebug
+add_lunch_combo commotio_maguro-userdebug
+add_lunch_combo commotio_toroplus-userdebug
+add_lunch_combo commotio_toro-user
+add_lunch_combo commotio_maguro-user
+add_lunch_combo commotio_toroplus-user
+add_lunch_combo commotio_grouper-user
