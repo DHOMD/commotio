0a040415faad6d1ed9fe4815d59b1500ae249e69
DHO <DHO@dho.im>
2013-03-13 22:53:09 -0400

    Bump, update DCW, update OTA
    Work art

--- products/common.mk
+++ products/common.mk
@@ -43,8 +43,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     TCHTHR=28 \
     ro.rommanager.developerid=DHO \
     ro.goo.developerid=DHO \
-    ro.modversion=Commotio-B43 \
-    ro.goo.version=43
+    ro.modversion=Commotio-B44 \
+    ro.goo.version=44
 
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JDQ39 BUILD_ID=JDQ39 BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
