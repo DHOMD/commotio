138a180b12073cf8226911de67f65cc10e17b1d4
DHOMD <DHO@dho.im>
2013-06-09 16:29:28 -0400

    Bump + i605 fingerprint

--- products/common.mk
+++ products/common.mk
@@ -46,8 +46,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     TCHTHR=28 \
     ro.rommanager.developerid=DHO \
     ro.goo.developerid=DHO \
-    ro.modversion=Commotio-B51 \
-    ro.goo.version=51
+    ro.modversion=Commotio-B52 \
+    ro.goo.version=52
 
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JDQ39 BUILD_ID=JDQ39 BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
