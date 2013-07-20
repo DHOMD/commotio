23b9c8be943317d8dc629f0b422eb38ae4a2e112
DHOMD <DHO@dho.im>
2013-07-19 23:27:41 -0400

    Bump 57

--- products/common.mk
+++ products/common.mk
@@ -47,8 +47,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     TCHTHR=28 \
     ro.rommanager.developerid=DHO \
     ro.goo.developerid=DHO \
-    ro.modversion=Commotio-B56 \
-    ro.goo.version=56
+    ro.modversion=Commotio-B57 \
+    ro.goo.version=57
 
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JDQ39 BUILD_ID=JDQ39 BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
