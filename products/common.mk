5d7918d4e33b095d688d0bcb702714dcf403044d
DHOMD <DHO@dho.im>
2013-08-06 18:46:12 -0400

    Reconcile with Vanir

--- products/common.mk
+++ products/common.mk
@@ -12,7 +12,8 @@ PRODUCT_PACKAGES += \
     Babel \
     busybox \
     DSPManager \
-	Email
+	Email \
+	Focal
 
 # Build Properties
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -26,7 +27,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enterprise_mode=1 \
     ro.config.ringtone=CanisMajor.ogg \
     ro.config.notification_sound=Proxima.ogg \
-    ro.config.alarm_alert=Cesium.ogg 
+    ro.config.alarm_alert=Cesium.ogg \
+    ro.build.selinux=1
 
 # Build.Prop Tweaks
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -53,47 +55,54 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JDQ39 BUILD_ID=JDQ39 BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
 
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
+    vendor/vanir/proprietary/common/xbin/sysro:system/xbin/sysro \
+    vendor/vanir/proprietary/common/xbin/vanirinteractivegovernorgovernor:system/xbin/vanirinteractivegovernorgovernor \
+    vendor/vanir/proprietary/common/xbin/vanirflash:system/xbin/vanirflash \
+    vendor/vanir/proprietary/common/init.vanir.rc:root/init.vanir.rc \
+    vendor/vanir/proprietary/common/bin/sysinit:system/bin/sysinit \
+    vendor/vanir/proprietary/common/xbin/purgethumbnails:system/xbin/purgethumbnails
+
+PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/vanir/proprietary/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinime.so
+
+ifeq ($(VANIR_FAILSAFE),)
 # Blobs common to all devices
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/bin/fix_permissions:system/bin/fix_permissions \
-    vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
-    vendor/vanir/proprietary/common/xbin/sysro:system/xbin/sysro \
+    vendor/vanir/proprietary/common/xbin/hunter:system/xbin/hunter \
     vendor/vanir/proprietary/common/xbin/testinitd:system/xbin/testinitd \
     vendor/vanir/proprietary/common/xbin/vanircheckcpu:system/xbin/vanircheckcpu \
     vendor/vanir/proprietary/common/xbin/vanirtweakapply:system/xbin/vanirtweakapply \
     vendor/vanir/proprietary/common/xbin/vanir:system/xbin/vanir \
-    vendor/vanir/proprietary/common/xbin/vanirflash:system/xbin/vanirflash \
-    vendor/commotio/proprietary/system/xbin/vanirnice:system/xbin/vanirnice \
-    vendor/vanir/proprietary/common/xbin/vanirinteractivegovernorgovernor:system/xbin/vanirinteractivegovernorgovernor
+    vendor/commotio/proprietary/common/xbin/vanirnice:system/xbin/vanirnice
 
 # Imoseyon's zram script
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/zram:system/xbin/zram
 
 # Misc Files
-PRODUCT_COPY_FILES +=  \
+PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/hosts:system/etc/hosts \
     vendor/vanir/proprietary/common/etc/resolv.conf:system/etc/resolv.conf
 
-# bash stuff... config files and so's
-PRODUCT_COPY_FILES +=  \
-    vendor/vanir/proprietary/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
-    vendor/vanir/proprietary/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinime.so \
+# proprietary guts
+PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/vanir/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
-# init.vanir.rc, 2 copies of it for compatibility, and some shell scripts from CM
-PRODUCT_COPY_FILES +=  \
-    vendor/vanir/proprietary/common/init.vanir.rc:root/init.vanir.rc \
-    vendor/vanir/proprietary/common/bin/sysinit:system/bin/sysinit \
-    vendor/vanir/proprietary/common/bin/cronlogger:system/bin/cronlogger \
+# entropy mumbo jump
+PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/CB_RunHaveged:system/xbin/CB_RunHaveged \
     vendor/vanir/proprietary/common/xbin/haveged:system/xbin/haveged \
-    vendor/vanir/proprietary/common/xbin/hunter:system/xbin/hunter \
-    vendor/vanir/proprietary/common/xbin/vanirentropy:system/xbin/vanirentropy 
+    vendor/vanir/proprietary/common/xbin/vanirentropy:system/xbin/vanirentropy \
+    vendor/vanir/proprietary/common/bin/cronlogger:system/bin/cronlogger
 
 # init.d Tweaks
 PRODUCT_COPY_FILES +=  \
+    vendor/commotio/proprietary/common/etc/sysctl.conf:system/etc/sysctl.conf \
     vendor/commotio/proprietary/system/etc/init.d/01dhotheory:system/etc/init.d/01dhotheory \
     vendor/commotio/proprietary/system/etc/init.d/03dhokernel:system/etc/init.d/03dhokernel \
     vendor/vanir/proprietary/common/etc/init.d/06ENTROPY:system/etc/init.d/06ENTROPY \
@@ -142,7 +151,7 @@ PRODUCT_PACKAGES += \
     fsck.exfat \
     mkfs.exfat \
     ntfsfix \
-    ntfs-3g 
+    ntfs-3g
 
 # Openssh
 PRODUCT_PACKAGES += \
@@ -157,13 +166,13 @@ PRODUCT_PACKAGES += \
 # rsync
 PRODUCT_PACKAGES += \
     rsync
-    
+
 # T-Mobile theme engine
 PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes \
-       Vanir_Inverted
+    ThemeManager \
+    ThemeChooser \
+    com.tmobile.themes \
+    Vanir_Inverted
 
 PRODUCT_COPY_FILES += \
        vendor/vanir/proprietary/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
