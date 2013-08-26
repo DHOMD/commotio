e148d5fa4095b0f2059460a2f9a8ab2d549d18c8
DHOMD <DHO@dho.im>
2013-08-26 18:08:47 -0400

    yuga 2/2

---
+++ products/commotio_yuga.mk
@@ -0,0 +1,36 @@
+#Squisher Choosing
+DHO_VENDOR := commotio
+
+# Commotio
+$(call inherit-product, vendor/commotio/products/common_commotio.mk)
+$(call inherit-product, vendor/commotio/products/common_phones.mk)
+$(call inherit-product, vendor/vanir/products/gsm.mk)
+
+#Commotio theme files
+    PRODUCT_PACKAGE_OVERLAYS += vendor/commotio/overlay/theme
+
+# Vanir Config = awesome
+PRODUCT_COPY_FILES +=  \
+    vendor/commotio/proprietary/system/etc/vanir.cfg:system/etc/vanir.cfg
+
+# Inherit AOSP device configuration for yuga.
+$(call inherit-product, device/sony/yuga/full_yuga.mk)
+
+# Bootanimation
+# PRODUCT_COPY_FILES +=  \
+#   vendor/commotio/proprietary/system/media/bootanimation.zip:system/media/bootanimation.zip
+
+#... and then make the AOSP device configuration for yuga get on its knees and sing the national anthem into our "microphones"
+PRODUCT_LOCALES := en_US
+
+# Setup device specific product configuration.
+PRODUCT_NAME := commotio_yuga
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := yuga
+PRODUCT_RELEASE_NAME := Xperia Z
+PRODUCT_MODEL := c6606
+PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.3/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.3 10.3.A.0.423 WP5_rg test-keys"
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=Commotio-yuga
