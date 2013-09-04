d5e7c6d3e46cf08aaf49ce1f11415e646ae78f4e
DHOMD <DHO@dho.im>
2013-09-04 14:55:19 -0400

    GS4 family

---
+++ products/commotio_jflteatt.mk
@@ -0,0 +1,21 @@
+#Squisher Choosing
+DHO_VENDOR := commotio
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.goo.rom=Commotio-jflteatt
+
+# Inherit AOSP device configuration for toro.
+$(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
+
+$(call inherit-product, vendor/commotio/products/commotio_jf.mk)
+$(call inherit-product, vendor/vanir/products/gsm.mk)
+
+#... and then make the AOSP device configuration for toro get on its knees and sing the national anthem into our "microphones"
+PRODUCT_LOCALES := en_US
+
+# Setup device specific product configuration.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.3/JSS15Q/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.3 JSS15Q I337UCUAMDL release-keys"
+
+PRODUCT_NAME := commotio_jflteatt
+PRODUCT_DEVICE := jflteatt
---
+++ products/commotio_jfltetmo.mk
@@ -0,0 +1,21 @@
+#Squisher Choosing
+DHO_VENDOR := commotio
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.goo.rom=Commotio-jfltetmo
+
+# Inherit AOSP device configuration for toro.
+$(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
+
+$(call inherit-product, vendor/commotio/products/commotio_jf.mk)
+$(call inherit-product, vendor/vanir/products/gsm.mk)
+
+#... and then make the AOSP device configuration for toro get on its knees and sing the national anthem into our "microphones"
+PRODUCT_LOCALES := en_US
+
+# Setup device specific product configuration.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=jfltetmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.3/JSS15Q/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.3 JSS15Q M919UVUAMDB release-keys"
+
+PRODUCT_NAME := commotio_jfltetmo
+PRODUCT_DEVICE := jfltetmo
