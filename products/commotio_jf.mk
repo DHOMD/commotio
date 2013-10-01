8b82db79549c6ff8504f08c360a5c7d96e2c19cf
DHOMD <DHO@dho.im>
2013-09-30 21:03:24 -0400

    Jf config, app updates

--- products/commotio_jf.mk
+++ products/commotio_jf.mk
@@ -12,4 +12,4 @@ $(call inherit-product, vendor/commotio/products/common_phones.mk)
 
 # Default values used by 99vanir. copied to /sdcard on first run, and presently it lives there forever... todo: make a runonce script that adds new default values to the copy on the sdcard as we add them or something
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/jf/etc/vanir.cfg:system/etc/vanir.cfg
+    vendor/commotio/proprietary/system/etc/jf_vanir.cfg:system/etc/vanir.cfg
