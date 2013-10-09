59b739d3239783744f9c9687e970a2cc42042018
DHOMD <DHO@dho.im>
2013-10-09 17:56:10 -0400

    4.3.1s

--- products/commotio_jflteatt.mk
+++ products/commotio_jflteatt.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/vanir/products/gsm.mk)
 PRODUCT_LOCALES := en_US
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.3/JLS36I/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.3 JLS36I I337UCUAMDL release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.3.1/JLS36I/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.3.1 JLS36I I337UCUAMDL release-keys"
 
 PRODUCT_NAME := commotio_jflteatt
 PRODUCT_DEVICE := jflteatt
