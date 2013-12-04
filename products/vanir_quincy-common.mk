bc21cd2db2141127f936b26b7d23a27c5244345f
DHOMD <DHO@dho.im>
2013-12-03 19:02:53 -0500

    Renaming

--- products/vanir_quincy-common.mk
+++
@@ -1,8 +0,0 @@
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/commotio/proprietary/boot_animations/800x1280.zip:system/media/bootanimation.zip
-
-# Vanir configuration
-$(call inherit-product, vendor/commotio/products/common_phones.mk)
- $(call inherit-product, vendor/commotio/products/beats.mk)
-$(call inherit-product, vendor/commotio/products/gsm.mk)
