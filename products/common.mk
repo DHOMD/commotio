480ac127b19c7aae42d3041c0ca94c08adfa751c
DHOMD <DHO@dho.im>
2013-12-04 17:50:06 -0500

    use vanir for su package

--- products/common.mk
+++ products/common.mk
@@ -128,7 +128,7 @@ endif
 
 #Define thirdparty for Koush's SU
 SUPERUSER_EMBEDDED := true
-SUPERUSER_PACKAGE_PREFIX := com.android.settings.commotio.superuser
+SUPERUSER_PACKAGE_PREFIX := com.android.settings.vanir.superuser
 
 # Required CM packages
 PRODUCT_PACKAGES += \
