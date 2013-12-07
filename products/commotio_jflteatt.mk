4c153344a492ec412e04129f4c73d94fed64f78a
DHOMD <DHO@dho.im>
2013-12-07 13:01:27 -0500

    4.1

--- products/commotio_jflteatt.mk
+++ products/commotio_jflteatt.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/commotio/products/commotio_jf-common.mk)
 $(call inherit-product, vendor/commotio/products/gsm.mk)
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.4/KOT49E/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.4 KOT49E I337UCUAMDL release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.4.1/KOT49E/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.4.1 KOT49E I337UCUAMDL release-keys"
 
 PRODUCT_NAME := commotio_jflteatt
 PRODUCT_DEVICE := jflteatt
