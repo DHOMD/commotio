df54a8a04b5f1a4cd38d07454f2105d38b4ba0dd
DHOMD <DHO@dho.im>
2013-12-30 21:15:47 -0500

    d710

--- products/commotio_d710.mk
+++ products/commotio_d710.mk
@@ -1,25 +1,26 @@
-#
-# Copyright (C) 2012 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#Squisher Choosing
+DHO_VENDOR := commotio
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    ro.goo.rom=Commotio-d710
 
+# Boot Animation
+PRODUCT_COPY_FILES += \
+    vendor/commotio/proprietary/boot_animations/480x800.zip:system/media/bootanimation.zip
 
+# Inherit device configuration
 $(call inherit-product, device/samsung/d710/full_d710.mk)
+
+# Inherit common commotio files.
 $(call inherit-product, vendor/commotio/products/common_phones.mk)
 $(call inherit-product, vendor/commotio/products/cdma.mk)
 
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d710
 PRODUCT_NAME := commotio_d710
+PRODUCT_BRAND := Samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := SPH-D710 
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SPH-D710 TARGET_DEVICE=SPH-D710 BUILD_FINGERPRINT=samsung/SPH-D710/SPH-D710:4.4.2/JZO54K/SPH-D710.GB27:user/release-keys PRIVATE_BUILD_DESC="SPH-D710-user 4.4.2 JZO54K SPH-D710.GB27 release-keys"
