9449cc437f5773217d8ca74335f21e5062d07c89
DHOMD <DHO@dho.im>
2014-01-05 11:42:23 -0500

    Remove ZRAM from common

--- products/common.mk
+++ products/common.mk
@@ -88,10 +88,6 @@ PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/vanircheckcpu:system/xbin/vanircheckcpu \
     vendor/commotio/proprietary/system/xbin/vanirnice:system/xbin/vanirnice
 
-#Imoseyon's zram script
-PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/common/xbin/zram:system/xbin/zram
-
 # Misc Files
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/hosts:system/etc/hosts \
