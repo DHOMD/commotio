c0cd0ef94f5294bbdb1a55051e9e6154fd3a9a90
DHOMD <DHO@dho.im>
2014-02-02 13:22:44 -0500

    Test htlte unified

--- products/commotio_hltetmo.mk
+++ products/commotio_hltetmo.mk
@@ -3,26 +3,26 @@ DHO_VENDOR := commotio
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true \
-    ro.goo.rom=Commotio-hltetmo
+    ro.goo.rom=commotio-hlte
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    vendor/commotio/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+    vendor/vanir/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/hltetmo/full_hltetmo.mk)
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
-# Inherit common commotio files.
+# Inherit common vanir files.
 $(call inherit-product, vendor/commotio/products/common_phones.mk)
 $(call inherit-product, vendor/commotio/products/gsm.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := hltetmo
-PRODUCT_NAME := commotio_hltetmo
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := commotio_hlte
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := SM-N900T
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hltetmo TARGET_DEVICE=hltetmo BUILD_FINGERPRINT="samsung/hltetmo/hltetmo:4.4.2/KOT49H/N900TUVUBMI7:user/release-keys" PRIVATE_BUILD_DESC="hltetmo-user 4.4.2 KOT49H N900TUVUBMI7 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hlte TARGET_DEVICE=hlte BUILD_FINGERPRINT="samsung/hlte/hlte:4.4.2/KOT49H/N900TUVUBMI7:user/release-keys" PRIVATE_BUILD_DESC="hlte-user 4.4.2 KOT49H N900TUVUBMI7 release-keys"
