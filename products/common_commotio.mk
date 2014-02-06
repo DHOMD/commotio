976ac33e1a09ca47b07de7ec134a3260a9a09667
DHOMD <DHO@dho.im>
2014-02-06 14:07:45 -0500

    Set persist.sys.dun.override=0.... for now

--- products/common_commotio.mk
+++ products/common_commotio.mk
@@ -16,6 +16,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.alarm_alert=Cesium.ogg \
     ro.config.vc_call_vol_steps=9 \
     ro.telephony.call_ring.delay=50 \
+    persist.sys.dun.override=0 \
     persist.sys.camera-sound=0 
 
 # Video - Photo
