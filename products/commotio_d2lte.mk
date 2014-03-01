f60d9c58ee4ad4f4f2c294c6b2a8a0aaef79257f
DHOMD <DHO@dho.im>
2014-03-01 11:22:16 -0500

    Reconcile d2lte with vanir

--- products/commotio_d2lte.mk
+++ products/commotio_d2lte.mk
@@ -6,8 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
-
+    vendor/commotio/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
 # Inherit common phone stuff
 $(call inherit-product, vendor/commotio/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
@@ -15,7 +14,9 @@ $(call inherit-product, vendor/vanir/products/beats.mk)
 $(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/commotio/config/nfc_enhanced.mk)
+
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
