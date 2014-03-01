9f08812d75674a6c46a7befeac96cd402b0b729d
DHOMD <DHO@dho.im>
2014-03-01 11:25:45 -0500

    Clean up common phone

--- products/common_phones.mk
+++ products/common_phones.mk
@@ -2,22 +2,17 @@
 $(call inherit-product, vendor/commotio/products/common_commotio.mk)
 $(call inherit-product, vendor/commotio/products/common.mk)
 
-# World APN list
+# World APN + SPN list
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
+    vendor/vanir/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/vanir/proprietary/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml \
     vendor/vanir/proprietary/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk
 
 PRODUCT_PACKAGES += \
     SpeakerProximity \
     CellBroadcastReceiver \
     Mms \
+    Stk \
     Torch
 
 # Audio Packages
