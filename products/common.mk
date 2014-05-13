70a41e1563b8d9ed1ce4ac5b416020cd600d12a5
DHOMD <DHO@dho.im>
2014-05-12 22:15:25 -0400

    Test dexproopt for all

--- products/common.mk
+++ products/common.mk
@@ -11,6 +11,9 @@ $(call inherit-product, vendor/vanir/config/nfc_enhanced.mk)
 # Add some tones (if this grows to more than like... 5 ringtones and 5 notifications, old ones will be dropped)
 $(call inherit-product, vendor/vanir/proprietary/ringtones/VanirRingtones.mk)
 
+#Dexpreopt to all builds
+WITH_DEXPREOPT := true 
+
 # Build packages included in manifest
 PRODUCT_PACKAGES += \
     busybox \
