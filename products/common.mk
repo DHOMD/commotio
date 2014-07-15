8d855ac746ea13ecaff6dfe168baa116cd850d1d
DHOMD <DHO@dho.im>
2014-07-15 19:09:08 -0400

    KTU84Q

--- products/common.mk
+++ products/common.mk
@@ -55,7 +55,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.version=68
 
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=KTU84P BUILD_ID=KTU84P BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=KTU84Q BUILD_ID=KTU84Q BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
 
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
