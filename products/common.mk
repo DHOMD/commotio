39e1ebc4e9356ed1920b11785112475ac38d1429
DHOMD <DHO@dho.im>
2014-08-24 11:47:09 -0400

    Staging for 70

--- products/common.mk
+++ products/common.mk
@@ -51,8 +51,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vanir.base=4.4.4 \
     ro.rommanager.developerid=DHO \
     ro.goo.developerid=DHO \
-    ro.modversion=Commotio-B69 \
-    ro.goo.version=69
+    ro.modversion=Commotio-B70 \
+    ro.goo.version=70
 
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=KTU84Q BUILD_ID=KTU84Q BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
