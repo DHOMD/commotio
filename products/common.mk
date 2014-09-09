c914614b58e00a5fc63abeb27a558c2765c86487
DHOMD <DHO@dho.im>
2014-09-09 19:22:48 -0400

    Let b71 start

--- products/common.mk
+++ products/common.mk
@@ -52,8 +52,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vanir.base=4.4.4 \
     ro.rommanager.developerid=DHO \
     ro.goo.developerid=DHO \
-    ro.modversion=Commotio-B70 \
-    ro.goo.version=70
+    ro.modversion=Commotio-B71 \
+    ro.goo.version=71
 
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=KTU84Q BUILD_ID=KTU84Q BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
