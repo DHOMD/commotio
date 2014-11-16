29d53e8bf6bb59a6d7b993e5614e99e9578e60f0
DHOMD <DHO@dho.im>
2014-11-16 14:56:14 -0500

    Update 44 --> 5

--- products/commotio_m7vzw.mk
+++ products/commotio_m7vzw.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/commotio/products/cdma.mk)
 $(call inherit-product, vendor/vanir/products/vzw.mk)
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=LRX21T BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.4.4/LRX21T/304035.8:user/release-k" PRIVATE_BUILD_DESC="3.11.605.1 CL304035 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=LRX21T BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:5.0/LRX21T/304035.8:user/release-k" PRIVATE_BUILD_DESC="3.11.605.1 CL304035 release-keys"
 
 PRODUCT_NAME := commotio_m7vzw
 PRODUCT_DEVICE := m7vzw
