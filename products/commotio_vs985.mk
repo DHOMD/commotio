2d9a9810b73816770bf9c2f3d40d4019aa0bc43d
DHOMD <DHO@dho.im>
2014-11-17 19:41:25 -0500

    Use my template FTW

--- products/commotio_vs985.mk
+++ products/commotio_vs985.mk
@@ -1,23 +1,23 @@
-# when this builds, it will probably mess your phone up.
-# you might want to wait to try this unless you have a giant pair of low-hanging huevos
+#Squisher Choosing
+DHO_VENDOR := commotio
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
     ro.goo.rom=commotio-vs985
-
-$(call inherit-product, device/lge/vs985/full_vs985.mk)
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/commotio/proprietary/boot_animations/1440x2560.zip:system/media/bootanimation.zip
 
-# Inherit common product files.
-NO_DRM_BLOBS := true
-$(call inherit-product, vendor/vanir/products/beats.mk)
-$(call inherit-product, vendor/vanir/products/vzw.mk)
-$(call inherit-product, vendor/commotio/products/cdma.mk)
+# Inherit device configuration
+$(call inherit-product, device/lge/vs985/full_vs985.mk)
 
-# Inherit common phone stuff
+# Inherit common commotio files.
 $(call inherit-product, vendor/commotio/products/common_phones.mk)
+$(call inherit-product, vendor/commotio/products/cdma.mk)
+$(call inherit-product, vendor/vanir/products/vzw.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs985
 PRODUCT_NAME := commotio_vs985
+
