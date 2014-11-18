88cc396f2b99a1268c07e1b074ea793b484b71d4
DHOMD <DHO@dho.im>
2014-11-17 19:40:38 -0500

    Nothing works here, use my template FTW

--- products/commotio_vs985.mk
+++ products/commotio_vs985.mk
@@ -1,25 +1,23 @@
-# when this builds, it will probably mess your phone up.
-# you might want to wait to try this unless you have a giant pair of low-hanging huevos
+#Squisher Choosing
+DHO_VENDOR := commotio
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.rom=commotio-vs985 \
-    ro.com.google.clientidbase=android-samsung \
-    ro.com.google.clientidbase.gmm=android-samsung
-
-$(call inherit-product, device/lge/vs985/full_vs985.mk)
+    drm.service.enabled=true \
+    ro.goo.rom=commotio-vs985
 
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/commotio/proprietary/boot_animations/1440x2560.zip:system/media/bootanimation.zip
 
-# Inherit common product files.
-NO_DRM_BLOBS := true
-$(call inherit-product, vendor/vanir/products/beats.mk)
-$(call inherit-product, vendor/vanir/products/vzw.mk)
-$(call inherit-product, vendor/commotio/products/cdma.mk)
+# Inherit device configuration
+$(call inherit-product, device/lge/vs985/full_vs985.mk)
 
-# Inherit common phone stuff
+# Inherit common commotio files.
 $(call inherit-product, vendor/commotio/products/common_phones.mk)
+$(call inherit-product, vendor/commotio/products/cdma.mk)
+$(call inherit-product, vendor/vanir/products/vzw.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vs985
 PRODUCT_NAME := commotio_vs985
+
