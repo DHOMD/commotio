f71b97fef8643cc7e4af28e1b33dd30e19707b10
DHOMD <DHO@dho.im>
2014-11-19 20:26:19 -0500

    Mild cleanup

--- products/commotio_tuna.mk
+++ products/commotio_tuna.mk
@@ -1,6 +1,3 @@
-# Variable for zip installerscript spam about kernel
-KERNEL_SPAM := Vanir Kernel v12 just rolled up in a retro custom van with an airbrushed Dan Trifan the Wizard on the side
-
 # Boot Animation
 PRODUCT_COPY_FILES += \
     vendor/commotio/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
@@ -10,7 +7,3 @@ $(call inherit-product, vendor/commotio/products/common_phones.mk)
 $(call inherit-product, vendor/vanir/products/beats.mk)
 
 $(call inherit-product, vendor/commotio/tweaks/tuna/tweaks.mk)
-
-# Extra Packages for Tunas
-PRODUCT_PACKAGES += \
-    Mira4U
