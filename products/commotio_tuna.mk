42d753ea4b4cc08e0050b905cabe2b0d55f4d6cc
DHOMD <DHO@dho.im>
2015-03-03 18:29:24 -0500

    Moar trash
    Change-Id: Icc820d099fdafe08ac234e49bf2ca25799554f3b

--- products/commotio_tuna.mk
+++
@@ -1,8 +0,0 @@
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/commotio/proprietary/boot_animations/720x1280.zip:system/media/bootanimation.zip
-
-# Inherit common product files.
-$(call inherit-product, vendor/commotio/products/common_phones.mk)
-
-$(call inherit-product, vendor/commotio/tweaks/tuna/tweaks.mk)
