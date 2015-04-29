15ade8a0b2b7e572139064a90f3707a59c70938a
DHOMD <DHO@dho.im>
2015-04-29 17:07:41 -0400

    Remove spn
    Change-Id: I1105072f9396fc6370331ee86214a5d1adbd09d0

--- products/common_phones.mk
+++ products/common_phones.mk
@@ -5,11 +5,9 @@ $(call inherit-product, vendor/commotio/products/common.mk)
 # World APN + SPN list
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
-    vendor/vanir/proprietary/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml \
-    vendor/vanir/proprietary/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+    vendor/vanir/proprietary/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
 PRODUCT_PACKAGES += \
-    SpeakerProximity \
     CellBroadcastReceiver \
     Mms \
     Stk \
