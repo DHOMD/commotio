f65e0c75dbc2e60a7ccab83bad87b7445f9e7494
DHOMD <DHO@dho.im>
2015-09-09 21:51:50 -0400

    And google does it yet again
    Change-Id: Ia1c6dd9c7407d2765873b14bc35ed04ecb9dd9d7

--- products/common.mk
+++ products/common.mk
@@ -68,7 +68,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.version=90
 
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=LMY48P BUILD_ID=LMY48P BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=LVY48F BUILD_ID=LVY48F BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_EST_DATE=$(shell date +"%s")
 
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
