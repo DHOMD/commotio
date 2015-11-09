19a11e9fbb8b7c95a235d15ba7bc5188dcead4d2
DHOMD <DHO@dho.im>
2015-11-09 17:24:58 -0500

    To 6.0 and beyond
    Change-Id: Ic96462765c2d758283bb58aeb5b3756475429fcf

--- products/commotio_m7vzw.mk
+++ products/commotio_m7vzw.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/commotio/products/cdma.mk)
 $(call inherit-product, vendor/vanir/products/vzw.mk)
 
 # Setup device specific product configuration.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=MDB08M BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:5.1/MDB08M/304035.8:user/release-k" PRIVATE_BUILD_DESC="3.11.605.1 CL304035 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=MDB08M BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:6.0/MDB08M/304035.8:user/release-k" PRIVATE_BUILD_DESC="3.11.606.0 CL304035 release-keys"
 
 PRODUCT_NAME := commotio_m7vzw
 PRODUCT_DEVICE := m7vzw
--- products/commotio_manta.mk
+++ products/commotio_manta.mk
@@ -26,6 +26,6 @@ PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:5.1/MDB08M/937116:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 5.1 MDB08M 937116 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:6.0/MDB08M/937116:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 6.0 MDB08M 937116 release-keys"
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=commotio-manta
