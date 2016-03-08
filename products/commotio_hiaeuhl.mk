3c7d40e4daf4feec2f1135a8c38e3fec8b2a9610
DHOMD <DHO@dho.im>
2016-03-07 19:29:02 -0500

    Tagging
    Change-Id: Ibfe92627471a1ddd019b5346719ba224d3b59c31

--- products/commotio_hiaeuhl.mk
+++ products/commotio_hiaeuhl.mk
@@ -20,8 +20,4 @@ BOARD_VENDOR := htc
 TARGET_VENDOR := htc
 PRODUCT_DEVICE := hiaeuhl
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="htc_hiaeuhl" \
-    PRODUCT_NAME="hiaeuhl_00401" \
-    BUILD_FINGERPRINT="htc/hiaeuhl_00401/htc_hiaeuhl:6.0/MRA58K/635503.7:user/release-keys" \
-    PRIVATE_BUILD_DESC="1.10.401.7 CL635503 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="htc_hiaeuhl" PRODUCT_NAME="hiaeuhl_00401" BUILD_FINGERPRINT="htc/hiaeuhl_00401/htc_hiaeuhl:6.0/MRA58K/635503.7:user/release-keys" PRIVATE_BUILD_DESC="1.10.401.7 CL635503 release-keys"
--- products/commotio_hiaeul.mk
+++ products/commotio_hiaeul.mk
@@ -20,8 +20,4 @@ BOARD_VENDOR := htc
 TARGET_VENDOR := htc
 PRODUCT_DEVICE := hiaeul
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="htc_hiaeul" \
-    PRODUCT_NAME="hiaeul_00617" \
-    BUILD_FINGERPRINT="htc/hiaeul_00617/htc_hiaeul:6.0.1/MMB29M/680768.4:user/release-keys" \
-    PRIVATE_BUILD_DESC="1.55.617.4 CL680768 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="htc_hiaeul" PRODUCT_NAME="hiaeul_00617" BUILD_FINGERPRINT="htc/hiaeul_00617/htc_hiaeul:6.0.1/MMB29M/680768.4:user/release-keys" PRIVATE_BUILD_DESC="1.55.617.4 CL680768 release-keys"
