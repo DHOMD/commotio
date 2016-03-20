ffc0df3a184807796f7f2d67e4a9868eb7070720
DHOMD <DHO@dho.im>
2016-03-20 12:10:33 -0400

    Fingerprinted
    Change-Id: I58d872eb9db126b178e5a0c7b927ec33534f4522

--- products/commotio_flounder.mk
+++ products/commotio_flounder.mk
@@ -42,4 +42,4 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
 TARGET_PREBUILT_KERNEL := false
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys" BUILD_ID=MMB29R
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29V/2554798:user/release-keys PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29V 2554798 release-keys" BUILD_ID=MMB29V
