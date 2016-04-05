a53be6f8fc045d8c237a64a8572468cccc6f098c
DHOMD <DHO@dho.im>
2016-04-05 13:46:01 -0400

    Fingerprinted
    Change-Id: I3502fc9e69a5c6b25878bf787e08dcdf425feaa7

--- products/commotio_angler.mk
+++ products/commotio_angler.mk
@@ -22,4 +22,4 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 TARGET_VENDOR := huawei
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=angler BUILD_FINGERPRINT=google/angler/angler:6.0.1/MHC19I/2590160:user/release-keys PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19I 2590160 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=angler BUILD_FINGERPRINT=google/angler/angler:6.0.1/MHC19Q/2705526:user/release-keys PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19Q 2705526 release-keys"
