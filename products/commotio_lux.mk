aa53cd00b7d93c4c4f7dda7b1a91b0c268df9bc7
DHOMD <DHO@dho.im>
2016-04-04 20:08:41 -0400

    Lux it
    Change-Id: I0c09fba5b5b77c534f620140f738fc119eecc5f9

---
+++ products/commotio_lux.mk
@@ -0,0 +1,24 @@
+#Squisher Choosing
+DHO_VENDOR := commotio
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.rom=commotio-lux
+
+PRODUCT_COPY_FILES += \
+    vendor/commotio/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/lux/full_lux.mk)
+
+# Inherit vanir phone stuff
+$(call inherit-product, vendor/commotio/products/common_phones.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := lux
+PRODUCT_NAME := commotio_lux
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := lux
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Moto X Play"
