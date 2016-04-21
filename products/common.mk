911c2ab6dd96425291d511c113f02c5142ed2571
DHOMD <DHO@dho.im>
2016-04-21 16:41:40 -0400

    Bump 106
    Change-Id: I8ef66308c4370ea5a1dee5ec10164a35e781c5f2

--- products/common.mk
+++ products/common.mk
@@ -69,8 +69,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vanir.base=6.0 \
     ro.rommanager.developerid=DHO \
     ro.goo.developerid=DHO \
-    ro.modversion=Commotio-B105 \
-    ro.goo.version=105
+    ro.modversion=Commotio-B106 \
+    ro.goo.version=106
 
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
