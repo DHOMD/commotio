11eba750a267bc8590452b487c47c334bb597b71
DHOMD <DHO@dho.im>
2016-05-27 18:57:31 -0400

    Bump
    Change-Id: I0dccde5bd8dc79f23b8cf2a489904208d37ada40

--- products/common.mk
+++ products/common.mk
@@ -75,8 +75,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vanir.base=6.0 \
     ro.rommanager.developerid=DHO \
     ro.goo.developerid=DHO \
-    ro.modversion=Commotio-B108 \
-    ro.goo.version=108
+    ro.modversion=Commotio-B109 \
+    ro.goo.version=109
 
 PRODUCT_COPY_FILES += \
     vendor/vanir/proprietary/common/xbin/sysrw:system/xbin/sysrw \
