7064332aa423a2dd75ea67a1c11744e4d6aca357
DHOMD <DHO@dho.im>
2016-07-19 17:40:17 -0400

    FIngerprints
    Change-Id: I8e1520face937b5a76e3e70e7ea5fed46bb469da

--- products/common_phones.mk
+++ products/common_phones.mk
@@ -4,8 +4,7 @@ $(call inherit-product, vendor/commotio/products/common.mk)
 
 # World APN + SPN list
 PRODUCT_COPY_FILES += \
-    vendor/vanir/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
-    vendor/vanir/proprietary/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+    vendor/vanir/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \
