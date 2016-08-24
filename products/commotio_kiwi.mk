911df35fc8a584dbf51caeb66dc7843636dab007
DHOMD <DHO@dho.im>
2016-08-23 21:50:17 -0400

    kiwi
    Change-Id: Ie2b50db50eeb7b7dfd8f50355bacc6abcf8c059b

---
+++ products/commotio_kiwi.mk
@@ -0,0 +1,18 @@
+#Squisher Choosing
+DHO_VENDOR := commotio
+
+# Boot Animation
+PRODUCT_COPY_FILES += vendor/commotio/proprietary/boot_animations/1080x1920.zip:system/media/bootanimation.zip
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
+
+# Inherit common files.
+$(call inherit-product, vendor/commotio/products/common_phones.mk)
+
+PRODUCT_NAME := commotio_kiwi
+PRODUCT_BRAND := HONOR
+PRODUCT_MODEL := KIW-L24
+BOARD_VENDOR := huawei
+
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
