b092d83833c5c1bd2011ed4c756627640b1c4ff0
DHOMD <DHO@dho.im>
2016-11-18 19:03:21 -0500

    Fingerprinted
    Change-Id: I6e4dca7fc76b64e83882c4da4ff41a4f1396a718

--- products/commotio_hammerhead.mk
+++ products/commotio_hammerhead.mk
@@ -21,4 +21,4 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30Y/3067468:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30Y 3067468 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30X/3237893:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30X 3237893 release-keys"
