92b613a3722c455f72019db9d9116a2be2ba8671
DHOMD <DHO@dho.im>
2016-12-11 11:07:28 -0500

    Regen Bacon mk
    Change-Id: Ie0f56d40e04e0d9855e6e923ec7060e0892b1a83

--- products/commotio_bacon.mk
+++ products/commotio_bacon.mk
@@ -8,19 +8,25 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 
 # Inherit device configuration
-$(call inherit-product, device/oneplus/bacon/full_bacon.mk)
+$(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit common commotio files.
 $(call inherit-product, vendor/commotio/products/common_phones.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := bacon
 PRODUCT_NAME := commotio_bacon
-PRODUCT_BRAND := oneplus
+PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := A0001
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := oneplus
+TARGET_VENDOR := oneplus
+TARGET_VENDOR_PRODUCT_NAME := bacon
+TARGET_VENDOR_DEVICE_NAME := A0001
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=oneplus/bacon/A0001:6.0.1/MHC19Q/ZNH2KAS1KN:user/release-keys PRIVATE_BUILD_DESC="bacon-user 6.0.1 MHC19Q ZNH2KAS1KN release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=oneplus/bacon/A0001:6.0.1/MHC19Q/ZNH2KAS1KN:user/release-keys \
+    PRIVATE_BUILD_DESC="bacon-user 6.0.1 MHC19Q ZNH2KAS1KN release-keys"
