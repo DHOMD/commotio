51e45a9e9b7300a33001f71d49a30672343a81e3
DHOMD <DHO@dho.im>
2016-12-20 16:50:54 -0500

    permission name="android.permission.READ_PHONE_STATE" fixed="false"/>
    Change-Id: Iac3351bb94eb22341a7cbbf2ed7c9d23c57323ff

--- products/common_phones.mk
+++ products/common_phones.mk
@@ -14,4 +14,7 @@ PRODUCT_PACKAGES += \
 # Audio Packages
 include frameworks/base/data/sounds/AudioPackage7.mk
 
+PRODUCT_COPY_FILES += \
+    vendor/vanir/config/permissions/qcom_ims.xml:system/etc/default-permissions/qcom_ims.xml
+
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
