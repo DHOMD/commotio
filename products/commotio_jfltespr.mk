b2a3804672f3f642f4da016005e0778c2065bc2d
DHOMD <DHO@dho.im>
2017-02-10 13:45:27 -0500

    FIngerprinted
    Change-Id: I540ea8fcc457e564a0fe4bdcac2c3e0b5fcd7822

--- products/commotio_jfltespr.mk
+++ products/commotio_jfltespr.mk
@@ -1,8 +1,7 @@
 #Squisher Choosing
 DHO_VENDOR := commotio
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.rom=commotio-jfltespr
+PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=commotio-jfltespr
 
 # Boot Animation
 TARGET_SCREEN_WIDTH := 1080
@@ -24,10 +23,6 @@ PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SPH-L720
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=jfltespr \
-    TARGET_DEVICE=jfltespr \
-    BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:5.0.1/LRX22C/L720VPUGOK3:user/release-keys" \
-PRIVATE_BUILD_DESC="jfltespr-user 5.0.1 LRX22C L720VPUGOK3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:5.0.1/LRX22C/L720VPUGOK3:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 5.0.1 LRX22C L720VPUGOK3 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
