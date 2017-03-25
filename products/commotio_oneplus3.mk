199b7521e522d0a866c7c1628f08c045fd6ac68a
DHOMD <DHO@dho.im>
2017-03-24 20:26:29 -0400

    1+3 fullbase tele
    Change-Id: I96745878e946cc993a5d82d416ca60c423950696

--- products/commotio_oneplus3.mk
+++ products/commotio_oneplus3.mk
@@ -7,6 +7,9 @@ PRODUCT_PROPERTY_OVERRIDES += ro.goo.rom=commotio-oneplus3
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 
+# Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit device configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
