a82fa809bd7745e0a0895ff8804835078e2bedad
DHOMD <DHO@dho.im>
2017-04-08 10:23:01 -0400

    lvm
    Change-Id: I3904e60787721fa4152775fc437010e0b28c0083

---
+++ products/commotio_find7lvm.mk
@@ -0,0 +1,28 @@
+#Squisher Choosing
+DHO_VENDOR := commotio
+
+PRODUCT_PROPERTY_OVERRIDES += drm.service.enabled=true ro.goo.rom=commotio-find7-lvm
+
+# Boot Animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/oppo/find7-lvm/find7-lvm.mk)
+
+# Inherit common commotio files.
+$(call inherit-product, vendor/commotio/products/common_phones.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := commotio_find7lvm
+PRODUCT_DEVICE := find7
+PRODUCT_BRAND := OPPO
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_MODEL := find7
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1.1/LMY47V/1441693976:user/release-keys PRIVATE_BUILD_DESC="msm8974-user 5.1.1 LMY47V 153 release-keys" TARGET_DEVICE="find7"
