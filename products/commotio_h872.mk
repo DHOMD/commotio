0f5edd7fb5ba4a23fa25ce8c15a08e36322d0aa4
DHOMD <DHO@dho.im>
2017-04-17 12:38:28 -0400

    Fingerprints
    Change-Id: Ia610f252a56ceb66c9e61fd7c77e1e36f26fc172

--- products/commotio_h872.mk
+++ products/commotio_h872.mk
@@ -25,8 +25,4 @@ PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H872
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="g6" \
-    PRODUCT_NAME="lucye_tmo_us" \
-    BUILD_FINGERPRINT="lge/lucye_tmo_us/lucye:7.0/NRD90U/17062223981e1:user/release-keys" \
-PRIVATE_BUILD_DESC="lucye_tmo_us-user 7.0 NRD90U 17062223981e1 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g6" PRODUCT_NAME="lucye_tmo_us" BUILD_FINGERPRINT="lge/lucye_tmo_us/lucye:7.0/NRD90U/17062223981e1:user/release-keys" PRIVATE_BUILD_DESC="lucye_tmo_us-user 7.0 NRD90U 17062223981e1 release-keys"
