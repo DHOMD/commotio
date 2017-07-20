40a2a87ab58fef1fed2181e0116b7fa2da1bbe1e
DHOMD <DHO@dho.im>
2017-07-20 17:55:26 -0400

    1+3 update
    Change-Id: Ifd3dbeb2367ec675c792861a4200480861a46253

--- products/commotio_oneplus3.mk
+++ products/commotio_oneplus3.mk
@@ -8,10 +8,10 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 
 # Inherit from those products. Most annoying first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit common oneplus2 files.
@@ -25,13 +25,5 @@ PRODUCT_BRAND := OnePlus
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-TARGET_VENDOR_PRODUCT_NAME := OnePlus3
-TARGET_VENDOR_DEVICE_NAME := OnePlus3
-
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus3 PRODUCT_NAME=OnePlus3
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=OnePlus/OnePlus3/OnePlus3:7.0/NRD90M/12251711:user/release-keys PRIVATE_BUILD_DESC="OnePlus3-user 7.0 NRD90M 89 dev-keys"
-
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
-
 TARGET_VENDOR := oneplus
+
