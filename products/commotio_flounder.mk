adfe722d87157f2b6cddcaad3519ee0b7e859fd6
DHOMD <DHO@dho.im>
2017-10-13 08:50:58 -0400

    Fingerprints
    Change-Id: Ife81ac9ed39755085514921decc6a240fec59c30

--- products/commotio_flounder.mk
+++ products/commotio_flounder.mk
@@ -40,4 +40,4 @@ PRODUCT_NAME := commotio_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N9F27H/4108833:user/release-keys PRIVATE_BUILD_DESC="volantis-user 7.1.1 N9F27H 4108833 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=flounder BUILD_FINGERPRINT=google/volantis/flounder:7.1.1/N9F27M/4333998:user/release-keys PRIVATE_BUILD_DESC="volantis-user 7.1.1 N9F27M 4333998 release-keys"
