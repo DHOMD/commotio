6e6f1833e17a645556c2157371f58c0ff5d95351
DHOMD <DHO@dho.im>
2017-12-21 12:15:30 -0500

    Fingerprinted
    Change-Id: I79c1ff36f84344fe62346df6f972e9d1e183a826

--- products/commotio_himaul.mk
+++ products/commotio_himaul.mk
@@ -19,4 +19,4 @@ BOARD_VENDOR := htc
 TARGET_VENDOR := htc
 PRODUCT_DEVICE := himaul
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="htc_himaulatt" PRODUCT_NAME="himaulatt_na_gen_unlock" BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:6.0/MRA58K/669696.12:user/release-keys" PRIVATE_BUILD_DESC="3.35.617.12 CL669696 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="htc_himaulatt" PRODUCT_NAME="himaulatt_na_gen_unlock" BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:7.0/NRD90M/870623.7:user/release-keys" PRIVATE_BUILD_DESC="4.27.502.7 CL870623 release-keys"
